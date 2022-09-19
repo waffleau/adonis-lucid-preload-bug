f6c01b7d40b7f934836c797affcf430b104a9254
Mathew Ellis <mathew@kismet.healthcare>
2022-09-19 11:33:25 +1000

    Ensure all ID columns are bigint

--- database/migrations/1663380187904_topics.ts
+++ database/migrations/1663380187904_topics.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.bigIncrements('id')
       table.string('name').notNullable().unique()
       table.timestamps()
     })
--- database/migrations/1663380305927_topics_categories.ts
+++ database/migrations/1663380305927_topics_categories.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.bigIncrements('id')
       table.bigInteger('topic_id').references('topics.id').notNullable().index()
       table.bigInteger('category_id').references('categories.id').notNullable().index()
       table.timestamps()
